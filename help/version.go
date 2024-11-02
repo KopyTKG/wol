46fb951bb5a6e03bc548dde9543016467e7a0057
Martin Kopecký <kopecky@thekrew.app>
2024-11-02 10:10:32 +0100

    v1.0.0

--- help/version.go
+++ help/version.go
@@ -2,7 +2,7 @@ package help
 
 import "fmt"
 
-var VERSION string = "v0.0.1"
+var VERSION string = "v1.0.0"
 
 func Version() {
     fmt.Printf("WoL %s\n", VERSION)
--- main.go
+++ main.go
@@ -6,33 +6,10 @@ import (
 	"log"
 	"net"
 	"os"
-	"strconv"
 	"strings"
 
 	"thekrew.app/WoL/help"
 )
-
-func assemble(mac []byte) []byte {
-	l := 102
-	msg := make([]byte, l)
-
-	for i := range int(6) {
-		msg[i] = 0xFF
-	}
-
-	for i := range int(l - 6) {
-		msg[i+6] = mac[i%6]
-	}
-
-	return msg
-}
-
-func join(arr []int, el string) (item string) {
-	for i := range len(arr) - 1 {
-		item += strconv.Itoa(arr[i]) + el
-	}
-	return item + strconv.Itoa(arr[len(arr)-1])
-}
 
 func main() {
 	args := os.Args[1:]
@@ -67,47 +44,69 @@ func processArguments(args []string, sMac *string) {
 			os.Exit(1)
 
 		default:
-			log.Fatalf("Unknowed switch found (%s) run \"wol -h\"", arg)
+			log.Fatalf("Unknowed switch found (%s) run \"wol -h\"\n", arg)
 			os.Exit(1)
 		}
 	}
 }
-
-func wol(sMac *string) {
-	slMac := strings.Split((*sMac), ":")
-	mac := make([]byte, 6)
+func assebleMac(slMac []string) (mac []byte) {
+	mac = make([]byte, 6)
 
 	if len(slMac) != 6 {
 		log.Fatalln("invalid MAC address format")
-		return
+		os.Exit(1)
 	}
 
 	for i, hexStr := range slMac {
 		b, err := hex.DecodeString(hexStr)
 		if err != nil {
-			log.Fatalf("invalid MAC address octet: %s", hexStr)
-			return
+			log.Fatalf("invalid MAC address octet: %s\n", hexStr)
+			os.Exit(1)
 		}
 		mac[i] = b[0]
 	}
+	return mac
+}
+
+func assemble(mac []byte) (msg []byte) {
+	l := 102
+	msg = make([]byte, l)
+
+	for i := range int(6) {
+		msg[i] = 0xFF
+	}
+
+	for i := range int(l - 6) {
+		msg[i+6] = mac[i%6]
+	}
+
+	return msg
+}
+
+
+func wol(sMac *string) {
+	mac := assebleMac(strings.Split(*sMac, ":"))	
+	msg := assemble(mac)
 
 
 	Baddr := net.UDPAddr{
 		Port: 40000,
 		IP:   net.ParseIP("255.255.255.255"),
 	}
+
 	conn, err := net.DialUDP("udp", nil, &Baddr)
 	if err != nil {
-		return
+		log.Fatalln("Error while connection to UDP node")
+		os.Exit(1)
 	}
 
-	msg := assemble(mac)
 
 	_, err = conn.Write(msg)
 	if err != nil {
-		fmt.Println("Error sending message:", err)
-		return
+		log.Fatalf("Error sending message: %s\n", err)
+		os.Exit(1)
 	}
-
+	
+	fmt.Printf("Sending WoL to %s\n", *sMac)
 	conn.Close()
 }
